6d2afae62312dc6053785f098f5e7c4d88648633
spunsosig <ikram15@live.co.uk>
2023-03-14 00:04:47 +0000

    snake now grows longer when eating food

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ window = pygame.display.set_mode((600, 500))
 x = 250
 y = 250
 rect = pygame.Rect(x, y, 25,25)
+snake_segments = [pygame.Rect(x, y, 25, 25)]
 
 # initialise colours
 red = (255, 0, 0)
@@ -76,17 +77,29 @@ while True:
             if event.key == pygame.K_RIGHT:
                 move_Right()
 
+    # Checks the collisions of the snake and food and respawns the food
     if rect.colliderect(pygame.Rect(food).inflate(10,10)):
         food_x = random.randint(0, 600)
         food_y = random.randint(0, 500)
         print("x = %s \n y= %s" % (food_x, food_y))
         food = pygame.Rect(food_x, food_y, 25, 25)
+        snake_segments.append(pygame.Rect(food))
 
     # Draw snake at every loop
     x += x_increment
     y += y_increment
     window.fill(black)
+    for segment in snake_segments:
+        pygame.draw.rect(window, green, segment)
     rect = pygame.draw.rect(window, green, (x, y, 25, 25))
+
+    # Update the position of the snake's body segments
+    for i in range(len(snake_segments) - 1, 0, -1):
+        snake_segments[i].x = snake_segments[i - 1].x
+        snake_segments[i].y = snake_segments[i - 1].y
+    snake_segments[0].x += x_increment
+    snake_segments[0].y += y_increment
+
     food = pygame.draw.rect(window, red, food)
     pygame.display.flip()
 
